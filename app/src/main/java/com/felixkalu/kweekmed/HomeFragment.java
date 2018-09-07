6f38ee773e38607ea111838a4cd25c23ee7f0c97
Felix kalu <flxkalu@yahoo.com>
2018-09-07 16:19:43 +0100

    adding the new files and correcting the erros git warned me about

--- app/src/main/java/com/felixkalu/kweekmed/HomeFragment.java
+++ app/src/main/java/com/felixkalu/kweekmed/HomeFragment.java
@@ -3,9 +3,14 @@ package com.felixkalu.kweekmed;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SearchView;
+
+import com.squareup.picasso.Picasso;
 
 
 /**
@@ -20,10 +25,37 @@ public class HomeFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
 
+        //initializing elements
+        ImageView imageView = (ImageView)v.findViewById(R.id.imageView);
+        ImageView symptomsCheckerImageView = (ImageView)v.findViewById(R.id.symptomsCheckerImageView);
+
+        SearchView searchView = (SearchView) v.findViewById(R.id.searchView1);
+
+        //searchview icon click listener
+       searchView.setOnSearchClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               //open the next fragment as it is in webMD app
+               Log.i("Note", "SearchView clicked");
+           }
+       });
+
+       //symptomschecker link
+        symptomsCheckerImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Note", "symptoms checker clicked");
+            }
+        });
+
+
+
+
+
+        return v;
+    }
 }
