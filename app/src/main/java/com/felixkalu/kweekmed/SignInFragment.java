c8341d490e984da09dc7876c675755563226d266
Felix kalu <flxkalu@yahoo.com>
2018-09-15 19:20:56 +0100

    the parse log in has been implemented in the log in page, some other modifications were also made to make it look better

--- app/src/main/java/com/felixkalu/kweekmed/SignInFragment.java
+++ app/src/main/java/com/felixkalu/kweekmed/SignInFragment.java
@@ -6,7 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 
 /**
@@ -25,7 +32,38 @@ public class SignInFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.fragment_sign_in, container, false);
 
+        final EditText loginUserNameEditText = (EditText)v.findViewById(R.id.loginUserNameEditText);
+        final EditText loginUserPasswordEditText = (EditText)v.findViewById(R.id.loginUserPasswordEditText);
+        Button signInButton = (Button)v.findViewById(R.id.signInButton);
+        TextView forgotPasswordTextView = (TextView)v.findViewById(R.id.forgotPasswordTextView);
         TextView signUpTextView = (TextView)v.findViewById(R.id.signUpTextView);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = loginUserNameEditText.getText().toString();
+                String password = loginUserPasswordEditText.getText().toString();
+                //check and be sure that username and password fields are not empty
+                if (!(userName.matches("") || password.matches(""))) {
+                    try {
+                        ParseUser.logInInBackground(userName, password, new LogInCallback() {
+                            public void done(ParseUser user, ParseException e) {
+                                if (user != null) {
+                                    Toast.makeText(getActivity(), "You have Signed in", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(getActivity(), "Error Signing in", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+                    } catch (Exception e) {
+                        Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(getActivity(), "Please Complete The Sign In Form", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
         signUpTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
